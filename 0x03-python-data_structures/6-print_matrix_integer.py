b7f54de5c7252a2b6180a57336bb4390b18d900a
Viet Tran <vietnamtran@gmail.com>
2020-05-05 16:28:43 +0000

    First commit with tested code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    
+    for i in range(len(matrix)):
+        m_len = len(matrix[i])
+        for j in range(m_len):
+            if (j < (m_len - 1)):
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]))
