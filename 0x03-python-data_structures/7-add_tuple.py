61e8ece57247decce860bf087ee00774d3b97c94
Viet Tran <vietnamtran@gmail.com>
2020-05-05 23:24:03 +0000

    Add another set of parenthesis to return

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,4 +14,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif (len(tuple_b) == 1):
         new_b = (tuple_b[0], 0)
 
-    return (new_a[0] + new_b[0], new_a[1] + new_b[1])
+    return ((new_a[0] + new_b[0], new_a[1] + new_b[1]))
