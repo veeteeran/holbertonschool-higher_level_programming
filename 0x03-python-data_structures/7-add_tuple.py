b6c1c6cebb72517b7737caa71fbbc4d7377bf594
Viet Tran <vietnamtran@gmail.com>
2020-05-05 21:47:58 +0000

    Delete last if/else condition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,9 +14,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif (len(tuple_b) == 1):
         new_b = (tuple_b[0], 0)
 
-    if (len(tuple_a) > 2):
-        new_a = (tuple_a[0], tuple_a[1])
-    elif (len(tuple_b) > 2):
-        new_b = (tuple_b[0], tuple_b[1])
-
     return (new_a[0] + new_b[0], new_a[1] + new_b[1])
