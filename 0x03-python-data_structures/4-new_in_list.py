365c29222f88fd7e27b27dba0196e055474e41b5
Viet Tran <vietnamtran@gmail.com>
2020-05-05 15:33:34 +0000

    First commit with tested code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+
+    list_copy = my_list[:]
+    if idx < 0 or idx > len(my_list):
+        return (None)
+
+    list_copy[idx] = element
+    return (list_copy)
