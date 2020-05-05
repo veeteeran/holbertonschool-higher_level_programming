b5a15c9a0c6a9276aea979a123c06e8a3448cbb2
Viet Tran <vietnamtran@gmail.com>
2020-05-05 23:27:18 +0000

    Trying anything

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,4 +14,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif (len(tuple_b) == 1):
         new_b = (tuple_b[0], 0)
 
-    return ((new_a[0] + new_b[0], new_a[1] + new_b[1]))
+    a = new_a[0] + new_b[0]
+    b = new_a[1] + new_b[1]
+    result = (a, b)
+
+    return (result)
