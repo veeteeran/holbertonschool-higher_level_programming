9062ce20fa9a9d9d99b39b5b862fc33fcfdea97b
Viet Tran <vietnamtran@gmail.com>
2020-05-05 21:29:59 +0000

    Reassign tuple to a new tuple variable

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-   
+
+    new_a = tuple_a
+    new_b = tuple_b
+
     if (len(tuple_a) == 0):
-        tuple_a = (0, 0)
+        new_a = (0, 0)
     elif (len(tuple_b) == 0):
-        tuple_b = (0, 0)
+        new_b = (0, 0)
 
     if (len(tuple_a) == 1):
-        tuple_a = (tuple_a[0], 0)
+        new_a = (tuple_a[0], 0)
     elif (len(tuple_b) == 1):
-        tuple_b = (tuple_b[0], 0)
+        new_b = (tuple_b[0], 0)
 
     if (len(tuple_a) > 2):
-        tuple_a = (tuple_a[0], tuple_a[1])
+        new_a = (tuple_a[0], tuple_a[1])
     elif (len(tuple_b) > 2):
-        tuple_b = (tuple_b[0], tuple_b[1])
+        new_b = (tuple_b[0], tuple_b[1])
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (new_a[0] + new_b[0], new_a[1] + new_b[1])
