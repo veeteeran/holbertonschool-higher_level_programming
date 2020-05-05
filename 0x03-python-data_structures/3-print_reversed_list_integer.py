057d6786285b2007a20aafaac13115c05850b9e4
Viet Tran <vietnamtran@gmail.com>
2020-05-05 17:15:09 +0000

    Add else condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if (my_list == []):
        print()
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    else:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
