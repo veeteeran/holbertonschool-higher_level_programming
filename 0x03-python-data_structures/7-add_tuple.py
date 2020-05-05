5f52707d558db96244fb8d7d37e481e670ae9396
Viet Tran <vietnamtran@gmail.com>
2020-05-05 23:33:16 +0000

    Fix style issue

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,7 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_a = (0, 0)
     elif (len(tuple_a) == 1):
         new_a = (tuple_a[0], 0)
-    
 
     if (len(tuple_b) == 0):
         new_b = (0, 0)
