093a13f69e6836705f29dea980d44c77d134455a
Viet Tran <vietnamtran@gmail.com>
2020-05-05 17:50:56 +0000

    First commit with tested code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    
+    max_int = 0
+    if (my_list):
+        for i in my_list:
+            if (i > max_int):
+                max_int = i
+        return (max_int)
+    return (None)
