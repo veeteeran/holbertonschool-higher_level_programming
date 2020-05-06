537f79f8c003cd0ada4c1ce2ceafafc4f97a63fa
Viet Tran <vietnamtran@gmail.com>
2020-05-06 02:12:49 +0000

    Get rid of malloc

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,16 +12,11 @@ size_t len_listint(const listint_t *h);
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int i, start, size, end;
-	int *array;
+	int i, start, end;
+	int array[4096];
 
 	if (*head == NULL || head == NULL)
 		return (1);
-
-	size = len_listint(*head);
-	array = malloc(sizeof(int) * size);
-	if (array == NULL)
-		return (0);
 
 	current = *head;
 	i = 0;
@@ -31,16 +26,14 @@ int is_palindrome(listint_t **head)
 		current = current->next;
 		i++;
 	}
-	end = size - 1;
-	for (start = 0; start < size / 2; start++, end--)
+	end = i - 1;
+	for (start = 0; start < i / 2; start++, end--)
 	{
 		if (array[start] != array[end])
 		{
-			free(array);
 			return (0);
 		}
 	}
-	free(array);
 	return (1);
 }
 
