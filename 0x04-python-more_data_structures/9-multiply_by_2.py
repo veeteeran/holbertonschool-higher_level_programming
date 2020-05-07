008282fc970da66acddb0d3af2cec2c648ff7888
Viet Tran <vietnamtran@gmail.com>
2020-05-07 20:29:48 +0000

    Add space after colon

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,6 @@ def multiply_by_2(a_dictionary):
     keys = a_dictionary.keys()
     values = a_dictionary.values()
     dbl_val = [x * 2 for x in values]
-    new_dict = {k:v for k, v in zip(keys, dbl_val)}
+    new_dict = {k: v for k, v in zip(keys, dbl_val)}
 
     return new_dict
