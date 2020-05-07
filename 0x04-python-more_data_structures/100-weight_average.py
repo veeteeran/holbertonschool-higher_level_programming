fa71739e31bde474a04544ce9ce7e70ef381f3ac
Viet Tran <vietnamtran@gmail.com>
2020-05-07 22:59:56 +0000

    Add condition for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,12 +6,15 @@ def weight_average(my_list=[]):
     n1 = 0
     n2 = 0
 
-    for i in my_list:
-        l1.append(i[0])
-        l2.append(i[1])
+    if my_list:
+        for i in my_list:
+            l1.append(i[0])
+            l2.append(i[1])
 
-    for a, b in zip(l1, l2):
-        n1 += a * b
-        n2 += b
+        for a, b in zip(l1, l2):
+            n1 += a * b
+            n2 += b
 
-    return n1 / n2
+        return n1 / n2
+
+    return 0
