1291f5be9392803208e6acfe610100975f01d49c
Viet Tran <vietnamtran@gmail.com>
2020-05-07 20:43:41 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+
+    if (a_dictionary):
+        best = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+        return best[0][0]
+
+    return None
