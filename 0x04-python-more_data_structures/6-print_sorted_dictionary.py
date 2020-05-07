2080607e7c56874ac6a9d5946ed8e87534a7c2f2
Viet Tran <vietnamtran@gmail.com>
2020-05-07 19:21:37 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+
+    items = a_dictionary.items()
+    sorted_items = sorted(items)
+
+    for k, v in sorted_items:
+        print("{}: {}".format(k, v))
