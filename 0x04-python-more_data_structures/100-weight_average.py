2774306ba714f52afd83e3d54f9467bc0f3ac0a6
Viet Tran <vietnamtran@gmail.com>
2020-05-07 22:57:51 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+
+    l1 = []
+    l2 = []
+    n1 = 0
+    n2 = 0
+
+    for i in my_list:
+        l1.append(i[0])
+        l2.append(i[1])
+
+    for a, b in zip(l1, l2):
+        n1 += a * b
+        n2 += b
+
+    return n1 / n2
