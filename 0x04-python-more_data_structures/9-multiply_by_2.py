883bd3e18d6f1c5c0c36446a1e6a383dd4ec6170
Viet Tran <vietnamtran@gmail.com>
2020-05-07 20:28:48 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+
+    keys = a_dictionary.keys()
+    values = a_dictionary.values()
+    dbl_val = [x * 2 for x in values]
+    new_dict = {k:v for k, v in zip(keys, dbl_val)}
+
+    return new_dict
