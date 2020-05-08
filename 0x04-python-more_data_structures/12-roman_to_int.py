fcfcb9874f28257ec9cdd9170356d5d5f98500e6
Viet Tran <vietnamtran@gmail.com>
2020-05-08 03:26:44 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    roman = dict(I=1, IV=4, V=5, X=10, XL=40, L=50,
+                XC=90, C=100, CD=400, D=500, CM=900, M=1000)
+
+    if type(roman_string) != str or None:
+        return 0
+
+    result = 0
+    if roman_string:
+        for i in roman_string:
+            result += roman[i]
+
+    return result
