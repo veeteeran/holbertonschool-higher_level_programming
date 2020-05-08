8b3a72b103156a9aef182b10a89a8bc6eec46caf
Viet Tran <vietnamtran@gmail.com>
2020-05-08 01:56:27 +0000

    First commit with tested code

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-void print_python_list(PyObject *p);
+def complex_delete(a_dictionary, value):
+
+    dict_copy = a_dictionary.copy()
+    for k, v in dict_copy.items():
+        if v == value:
+            a_dictionary.pop(k)
+
+    return a_dictionary
