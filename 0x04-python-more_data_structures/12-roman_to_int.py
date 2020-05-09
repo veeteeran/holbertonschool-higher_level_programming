a012fd237517690897c1154b4a8d97a6d00776af
Viet Tran <vietnamtran@gmail.com>
2020-05-09 13:58:38 +0000

    FIrst commit with tested code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = dict(I=1, IV=4, V=5, X=10, XL=40, L=50,
-                XC=90, C=100, CD=400, D=500, CM=900, M=1000)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     if type(roman_string) != str or None:
         return 0
 
     result = 0
-    if roman_string:
-        for i in roman_string:
-            result += roman[i]
+    length = len(roman_string)
+    for i in range(length):
+        num = roman.get(roman_string[i])
+        if i + 1 < length and roman.get(roman_string[i + 1]) > num:
+            result -= num
+        else:
+            result += num
 
     return result
