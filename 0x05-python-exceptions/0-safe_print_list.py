ffd2e21a5d2b66daa978b9f79bedaf337bce354f
Viet Tran <vietnamtran@gmail.com>
2020-05-18 14:31:21 +0000

    First commit with tested code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    try:
+        elements = 0
+        for i in range(x):
+            print(my_list[i], end='')
+            elements += 1
+        print()
+        return elements
+    except IndexError:
+        print()
+        return elements
