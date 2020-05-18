e6115a378da18a74658c489102696a97fa19f0ab
Viet Tran <vietnamtran@gmail.com>
2020-05-18 20:15:20 +0000

    First commit with tested code

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+
+    import sys
+
+    try:
+        return fct(args[0], args[1])
+    except (ZeroDivisionError, IndexError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return None
