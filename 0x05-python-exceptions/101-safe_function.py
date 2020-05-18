27af157ac4365b6ab23f6f884a6795a73c827716
Viet Tran <vietnamtran@gmail.com>
2020-05-18 21:14:51 +0000

    Except all exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ def safe_function(fct, *args):
     import sys
 
     try:
-        return fct(args[0], args[1])
-    except (ZeroDivisionError, IndexError, NameError) as err:
+        return fct(*args)
+    except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
         return None
