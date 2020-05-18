93d8a38f1df8308a8f45e4df29f99edbb12f487b
Viet Tran <vietnamtran@gmail.com>
2020-05-18 21:47:21 +0000

    Checked with dis

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+from dis import dis
 def magic_calculation(a, b):
-    result = 0 
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise IndexError('Too far')
-
-            result += (a ** b) / i
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
         except:
             result = b + a
-        finally:
-            return result
+    return result
+
+dis(magic_calculation)
