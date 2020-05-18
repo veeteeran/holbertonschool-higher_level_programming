699debedc8c121dee149e7819cfbc2ad1d4fad36
Viet Tran <vietnamtran@gmail.com>
2020-05-18 15:06:40 +0000

    First commit with tested code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    try:
+        elements = 0
+        for i in range(x):
+            if (type(my_list[i]) == int):
+                print("{:d}".format(my_list[i]), end="")
+            elements += 1
+        print()
+        return elements
+    except ValueError:
+        print()
+        print("list index out of range")
