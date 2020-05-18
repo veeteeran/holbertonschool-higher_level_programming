0a1e2e8e6a3cab74a8643246e88722a71d0cf291
Viet Tran <vietnamtran@gmail.com>
2020-05-18 18:38:47 +0000

    Delete colon on line 8

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             elements += 1
         except (ValueError, TypeError):
-            continue:
+            continue
     print()
     return elements
