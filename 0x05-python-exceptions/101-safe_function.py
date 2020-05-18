8e0eb0354a8cd143183e0c498cf8f5ad4dcffff3
Viet Tran <vietnamtran@gmail.com>
2020-05-18 20:18:04 +0000

    Add ValueError

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ def safe_function(fct, *args):
 
     try:
         return fct(args[0], args[1])
-    except (ZeroDivisionError, IndexError) as err:
+    except (ZeroDivisionError, IndexError, ValueError) as err:
         sys.stderr.write("Exception: {}\n".format(err))
         return None
