e51271f52b60c5550b4ce49290442e432e3902b4
Viet Tran <vietnamtran@gmail.com>
2020-05-18 16:51:51 +0000

    First commit with tested code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {:.1f}".format(result))
+        return result
