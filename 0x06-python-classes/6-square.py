c278ada91ee27e8e7ba2490cb2851c37b5e99556
Viet Tran <vietnamtran@gmail.com>
2020-05-19 22:39:13 +0000

    Add long condition, fix style

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,9 @@ class Square:
     @position.setter
     def position(self, value):
         """setter method property with type check"""
-        if (type(value) != tuple or len(value) != 2 or value[0] < 0 or value[1] < 0):
+        if type(value) != tuple or len(value) != 2 or value[0] < 0 \
+                or value[1] < 0 or type(value[0]) != int or type(value[1]) \
+                != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
