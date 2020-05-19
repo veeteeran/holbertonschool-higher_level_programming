1bd9a6fd1b8b7ac75ec41b6e5094056f90a3147b
Viet Tran <vietnamtran@gmail.com>
2020-05-19 19:24:20 +0000

    Add docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Square:
+    """ Creates an empty Square object"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """Creates a Square object"""
     def __init__(self, size):
+        """ Initializes a size to private attribute"""
         self.__size = size
