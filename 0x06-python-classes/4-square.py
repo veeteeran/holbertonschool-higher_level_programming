27936124a25c316628757b1821da7353b6903da9
Viet Tran <vietnamtran@gmail.com>
2020-05-19 19:54:24 +0000

    First commit with tested code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+class Square:
+    """Creates a Square object"""
+    def __init__(self, size=0):
+        """Initializes data"""
+        self.size = size
+
+    def area(self):
+        """Returns the area of Square object"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """getter method for Square object"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method with type and value check for Square"""
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
