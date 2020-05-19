10252f277ef7b140237947efcf5484fe36feeccb
Viet Tran <vietnamtran@gmail.com>
2020-05-19 19:38:29 +0000

    First commit with tested code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+class Square:
+    """Creates a Square object"""
+    def __init__(self, size=0):
+        """Initializes data, checks for proper type and input"""
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Returns the area of Square object"""
+        return self.__size * self.__size
