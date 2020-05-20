953abac69b38b2d1b100098f5082776f06c3577d
Viet Tran <vietnamtran@gmail.com>
2020-05-20 01:12:01 +0000

    Move condition checks around

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         """setter method property with type check"""
         if type(value) is not tuple or len(value) is not 2 or \
                 type(value[0]) is not int or type(value[1]) \
-                is not int value[0] < 0 or value[1] < 0:
+                is not int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
