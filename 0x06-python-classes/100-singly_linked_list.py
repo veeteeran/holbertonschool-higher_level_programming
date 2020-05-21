641e0f2c02c4660c66ddbf56e577b58304107737
Viet Tran <vietnamtran@gmail.com>
2020-05-21 13:30:10 +0000

    Fix pep8 issue

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -26,7 +26,7 @@ class Node:
     def next_node(self, value):
         if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value 
+        self.__next_node = value
 
 """Creates a Singly Linked List object"""
 
