dd427f8748fc4336cf84380131c6a63ab0947f95
Viet Tran <vietnamtran@gmail.com>
2020-05-27 15:32:26 +0000

    First commit with tested code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Docstring
+"""
+
+class LockedClass:
+    """Docstring"""
+
+    __slots__ = ['first_name']
+
+    def __init__(self, value=""):
+        self.first_name = value
+
