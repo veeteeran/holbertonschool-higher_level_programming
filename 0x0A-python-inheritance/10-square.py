b17f3af30b33fda9228bfd89f89c2e515b11a551
Viet Tran <vietnamtran@gmail.com>
2020-06-02 23:53:49 +0000

    Import task 9 instead of having code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,59 +1,8 @@
 #!/usr/bin/python3
-"""Class for base geometry"""
-
-
-class BaseGeometry:
-    """Class for base geometry"""
-    def area(self):
-        """
-        Unimplemented area method
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates an integer
-
-            Parameters:
-                name: a string
-                value: an int
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-"""Class for Rectangle inherits from BaseGeometry"""
-
-
-class Rectangle(BaseGeometry):
-    """Class for Rectangle inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """
-        Init method for Rectangle
-
-            Parameters:
-                width: width of Rectangle object
-                height: height of Rectangle object
-        """
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def area(self):
-        """Return area of Rectangle object"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Print description of Rectangle"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
 """Class Square inherits from Rectangle"""
 
 
+Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Class Square inherits from Rectangle"""
     def __init__(self, size):
