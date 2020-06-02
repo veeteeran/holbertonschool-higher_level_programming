0b1d6a95b70e645e4692f9cbabb9a4c43ac7a222
Viet Tran <vietnamtran@gmail.com>
2020-06-02 23:07:51 +0000

    Add Docstring outside methods

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Docstring for is_kind_of_class"""
+
+
 def is_kind_of_class(obj, a_class):
     """
     Return True if the object is an instance of, or if the object is
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Docstring for inherits from"""
+
+
 def inherits_from(obj, a_class):
     """
     Returns True if the object is an instance of a class, otherwise False
