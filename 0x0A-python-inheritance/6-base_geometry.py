f121edb61ba40c93ce7fcafee73f4cee2bd28e1b
Viet Tran <vietnamtran@gmail.com>
2020-06-02 17:41:40 +0000

    First commit with tested code

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Class for base geometry"""
+
+
+class BaseGeometry:
+    """Class for base geometry"""
+    def area(self):
+        """
+        Unimplemented area method
+        """
+        raise Exception("area() is not implemented")
