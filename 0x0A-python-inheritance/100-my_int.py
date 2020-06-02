9e4c6630f044717eaadfb1870fd9575cc11e9920
Viet Tran <vietnamtran@gmail.com>
2020-06-02 22:05:27 +0000

    First commit with tested code

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""MyInt Class inherits from int"""
+
+
+class MyInt(int):
+    """MyInt inherits from int"""
+
+    def __eq__(self, other):
+        """
+        Redefine equality to be unequal
+
+            Parameters:
+                self: instance of the object
+                other: other object to compare against
+        """
+        if isinstance(type(other), type(self)):
+            return self == other
+        return False
+
+    def __ne__(self, other):
+        """
+        Redefine equality to be unequal
+
+            Parameters:
+                self: instance of the object
+                other: other object to compare against
+        """
+        return not self.__eq__(other)
