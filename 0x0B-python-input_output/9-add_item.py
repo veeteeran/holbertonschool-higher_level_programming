7a141bb2c23f92b94b325f18b0cf12e0a815eda0
Viet Tran <vietnamtran@gmail.com>
2020-06-03 22:03:01 +0000

    Add newlines

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 import os
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
