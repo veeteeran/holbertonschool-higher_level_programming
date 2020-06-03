f91c8590f1670b60ed8d60e827c08d51322f0381
Viet Tran <vietnamtran@gmail.com>
2020-06-03 20:31:07 +0000

    First commit with tested code

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Docstring for class Student"""
+
+
+class Student:
+    """
+    Defines a student by first name, last name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Init method for Student
+
+            Parameters:
+                first_name: first name
+                last_name: last name
+                age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns the dictionary description
+        """
+        return self.__dict__
