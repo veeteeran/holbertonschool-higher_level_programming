a7db12f661f608d752219f0f07152e8aa9b4fcf9
Viet Tran <vietnamtran@gmail.com>
2020-06-03 19:28:54 +0000

    First commit with tested code

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+from sys import argv
+import os
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+try:
+    filename = "add_item.json"
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+
+save_to_json_file(my_list, filename)
