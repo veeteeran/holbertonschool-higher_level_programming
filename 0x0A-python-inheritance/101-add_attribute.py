12999cb2eebc81cd2feba5582feef480b472e89d
Viet Tran <vietnamtran@gmail.com>
2020-06-03 02:46:36 +0000

    First commit with tested code

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Docstring for add_attribute"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Add attribute, raise TypeError if not possible
+
+        Parameters:
+            obj: the object to add attribute
+            name: attribute name
+            value: value of atribute
+    """
+    if hasattr(obj, '__dict__'):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
+
