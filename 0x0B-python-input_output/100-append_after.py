b6c311d2430d8fe06741383cb1e6b1754f5ffc58
Viet Tran <vietnamtran@gmail.com>
2020-06-04 02:35:55 +0000

    First commit with tested code

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Docstring for append_after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a
+    specific string
+
+        Parameters:
+            filename: name of file
+            search_string: insert after this string
+            new_string: string to insert
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        my_list = list(f)
+
+    for i in range(len(my_list)):
+        if search_string in my_list[i]:
+            my_list.insert(i + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as nf:
+        nf.writelines(my_list)
+
+    return filename
