bdc410501dfc2925af1ef5d7cda4070d95b93e89
Viet Tran <vietnamtran@gmail.com>
2020-06-09 18:39:25 +0000

    Revert to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base:
             Parameter:
                 list_dictionaries: a list of dictionaries
         """
-        if list_dictionaries is None list_dictionary == []:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
