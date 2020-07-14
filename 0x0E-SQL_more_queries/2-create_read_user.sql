70dc2ecbddd89981dbb74b6b6406c0154340a50a
Viet Tran <vietnamtran@gmail.com>
2020-07-14 19:05:56 +0000

    Fix typo

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1
 -- Another fucking comment
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' INDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
