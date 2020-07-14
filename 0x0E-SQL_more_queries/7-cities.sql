31f6ae5a25a24b4af5770356019ab102648c9b74
Viet Tran <vietnamtran@gmail.com>
2020-07-14 20:11:57 +0000

    Attempt 1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,5 +2,5 @@
 -- Another fucking comment
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
-state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id),
+state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 name VARCHAR(256) NOT NULL);
