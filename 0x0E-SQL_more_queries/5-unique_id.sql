a2947647fb2ed7e920396a0b6e45e1cdb3ec3ee2
Viet Tran <vietnamtran@gmail.com>
2020-07-14 19:33:56 +0000

    Add UNIQUE

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates the table unique_id
 -- Another fucking comment
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
