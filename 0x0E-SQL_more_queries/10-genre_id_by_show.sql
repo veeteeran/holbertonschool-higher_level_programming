b1d9e1670368746d937c30f195b843ffd526717e
Viet Tran <vietnamtran@gmail.com>
2020-07-14 22:00:39 +0000

    Attempt 0

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows
+-- Another fucking comment
+SELECT tv_shows.title, tv_show_genres.genre._id
+	FROM tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	ORDER BY tv_shows.title, tv_show_genres.genre_id;
