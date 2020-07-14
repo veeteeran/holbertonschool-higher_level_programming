c493ea86f3d0d1518d602a3e467c9ae573906634
Viet Tran <vietnamtran@gmail.com>
2020-07-14 20:36:11 +0000

    Add subquerie

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- lists all the cities of California in the database hbtn_0d_usa
 -- Another fucking comment
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = 1 ORDER BY cities.id;
+SELECT id, name FROM hbtn_0d_usa.cities
+	WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
+	ORDER BY cities.id;
