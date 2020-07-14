d84622fe6412165197f99140cbab61046b331bbb
Viet Tran <vietnamtran@gmail.com>
2020-07-14 18:35:40 +0000

    Tired of this fucking shit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- creates the MySQL server user user_0d_1
+-- Another fucking comment
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_id'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_id'@'localhost';
 FLUSH PRIVILEGES;
