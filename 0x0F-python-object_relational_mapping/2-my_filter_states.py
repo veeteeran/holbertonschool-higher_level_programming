d80d3d1ea5cbe9fbfca964fb64774a1f5e23795f
Viet Tran <vietnamtran@gmail.com>
2020-08-07 16:24:29 +0000

    Initial commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Takes in argument, displays all values in states table"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name='{}'\
+            ORDER by id ASC".format(argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
