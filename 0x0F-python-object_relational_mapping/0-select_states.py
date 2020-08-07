f63e2aeccc035905daf87ec666e24105c18250df
Viet Tran <vietnamtran@gmail.com>
2020-08-07 15:22:48 +0000

    First commit with tested code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Docstring for task 0"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
