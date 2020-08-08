904897b474bd5efe63d03eec307997d3b2cc7374
Viet Tran <vietnamtran@gmail.com>
2020-08-08 18:06:05 +0000

    JUst guessing here

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
