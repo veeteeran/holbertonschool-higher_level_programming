dab8cb0b64a4d1ab7796cd241923a759869b455b
Viet Tran <vietnamtran@gmail.com>
2020-08-08 17:57:00 +0000

    Fix typo states not stats

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         if rowi[1][0] == 'N':
