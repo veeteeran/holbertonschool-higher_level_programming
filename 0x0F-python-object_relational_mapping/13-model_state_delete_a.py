253a9ceab0b2ef6011ee4b554e6c581f6657526b
Viet Tran <vietnamtran@gmail.com>
2020-08-08 16:14:25 +0000

    Add syncronize-session=False to delete

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Delete all State objects containing letter 'a' from db hbtn_0e_6_usa"""
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
-
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'.format
             (sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).filter(State.name.like('%a%'))
-    session.delete(result)
+    session.query(State).filter(State.name.like('%a%')).delete(
+                synchronize_session=False)
+    session.commit()
