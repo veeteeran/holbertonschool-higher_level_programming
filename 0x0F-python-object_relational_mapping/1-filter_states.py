308399b52435d0037ac7ddde16df62db92fb5bf4
Viet Tran <vietnamtran@gmail.com>
2020-08-08 18:20:39 +0000

    Going back to OG code add BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'
+                ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
 
     cur.close()
     db.close()
