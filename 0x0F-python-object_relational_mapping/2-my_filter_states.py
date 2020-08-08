5686a56e78fc803288869985ae21f3662c4481ec
Viet Tran <vietnamtran@gmail.com>
2020-08-08 18:01:58 +0000

    Delete ORDER BY

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name='{}'\
-            ORDER by states.id ASC".format(argv[4])
+    query = "SELECT * FROM states WHERE name='{}'".format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
