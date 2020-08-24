fa60386fe512716bd05951bb17d7e0117936b1c4
Viet Tran <vietnamtran@gmail.com>
2020-08-24 16:31:53 +0000

    Getting good output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+"""Takes in a URL, sends a request to the URL and displays
+    the body of the response 
+"""
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        req = Request(url, data)
+        with urlopen(req) as response:
+            read = response.read()
+            print(read.decode("UTF-8"))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
