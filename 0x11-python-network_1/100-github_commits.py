d699b3bf847613e54bf6a9681569af46930301c9
Viet Tran <vietnamtran@gmail.com>
2020-08-24 20:49:10 +0000

    Tested 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Takes your Github credentials (username and password) and uses
+   the Github API to display your id
+"""
+from requests import get, post
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, owner)
+    r = get(url)
+    commits = r.json()
+    for i in range(len(commits) - 11, len(commits)):
+        print('{} {}'.format(commits[i]['sha'],
+                             commits[i]['commit']['author']['name']))
