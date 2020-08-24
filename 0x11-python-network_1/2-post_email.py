5bbcd9df1589b884dfa1d0a1badbf4f649d1815a
Viet Tran <vietnamtran@gmail.com>
2020-08-24 16:02:30 +0000

    First commit with tested code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+"""Takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+"""
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = urlencode(values)
+    data = data.encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        read = response.read()
+        print(read.decode("UTF-8"))
