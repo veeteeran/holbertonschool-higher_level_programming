f81acaf1976681fa6c682c735fa260a3ce1886c6
Viet Tran <vietnamtran@gmail.com>
2020-08-24 15:28:03 +0000

    Getting what seems like proper output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from urllib.request import urlopen, Request
+"""Fetches https://intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(url) as response:
+        read = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(read)))
+        print('\t- content: {}'.format(read))
+        print('\t- utf8 content: {}'.format(read.decode("UTF-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from urllib.request import urlopen, Request
+from sys import argv
+"""Takes in a URL, sends a request to the URL and displays the value
+    of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        print(response.info()['X-Request-Id'])
