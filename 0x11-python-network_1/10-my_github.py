3b6c78a96513a59f927993123e6fac3d01acf10e
Viet Tran <vietnamtran@gmail.com>
2020-08-24 20:14:21 +0000

    Tested task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Takes your Github credentials (username and password) and uses
+   the Github API to display your id
+"""
+from requests import get, post
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = argv[1]
+    token = argv[2]
+    r = get(url, auth=(user, token))
+
+    try:
+        id = (r.json()['id'])
+        print(id)
+    except KeyError:
+        print('None')
