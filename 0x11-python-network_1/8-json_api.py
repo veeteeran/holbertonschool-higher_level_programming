15e39ce0347f35e37e90bbb54042fe653d89a330
Viet Tran <vietnamtran@gmail.com>
2020-08-24 19:50:59 +0000

    Tested code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Takes in a letter and sends a POST request to
+   http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    r = get(argv[1])
+    if r.status_code >= 400:
+            print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
