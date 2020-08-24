7dece43dcfb596b86410bfa40d674caa1ca8e9e1
Viet Tran <vietnamtran@gmail.com>
2020-08-24 18:22:37 +0000

    Use requests instead of urllib

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from requests import get
+from sys import argv
+"""Takes in a URL, sends a request to the URL and displays the value
+    of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    req = get(argv[1])    
+    print(req.headers['x-request-id'])
