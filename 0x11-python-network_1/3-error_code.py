3cbfe3bde4f0d8640ec497de5c38c04f025f7bc9
Viet Tran <vietnamtran@gmail.com>
2020-08-24 18:16:26 +0000

    import urlencode

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,13 +2,15 @@
 """Takes in a URL, sends a request to the URL and displays
     the body of the response
 """
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from urllib.error import HTTPError
+from urllib.parse import urlencode
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
     try:
+        req = Request(url)
         with urlopen(req) as response:
             read = response.read()
             print(read.decode("UTF-8"))
