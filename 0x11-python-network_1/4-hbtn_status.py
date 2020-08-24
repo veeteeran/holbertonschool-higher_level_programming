637f915ee8a21daa5c48ebda53abd729090773e9
Viet Tran <vietnamtran@gmail.com>
2020-08-24 16:36:04 +0000

    Getting good output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from urllib.request import urlopen, Request
+"""Fetches https://intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(url) as response:
+        read = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(read.decode("UTF-8"))))
+        print('\t- content: {}'.format(read.decode("UTF-8")))
