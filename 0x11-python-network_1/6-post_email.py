1da22eeb8c225fcf2033610e585435ae1c00daad
Viet Tran <vietnamtran@gmail.com>
2020-08-24 18:30:30 +0000

    Trying without testing lol

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and displays the value
+    of the X-Request-Id variable found in the header of the response.
+"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        url = argv[1]
+        email = {'email': argv[2]}
+        print(req.post(url, data=email))
+    except:
+        pass
