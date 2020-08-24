b5c1c6f36c4e1bd655fb24bed95f76a39d92597e
Viet Tran <vietnamtran@gmail.com>
2020-08-24 18:52:30 +0000

    Tested 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and displays the
+    body of the response
+"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        r = get(argv[1])
+        print(r.text)
+    except:
+        if r.status_code >= 400:
+            print('Error code: {}'.format(r.status_code))
