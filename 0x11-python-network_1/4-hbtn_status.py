9c35bd12c57fb0a99e02415463b0923cef457dc4
Viet Tran <vietnamtran@gmail.com>
2020-08-24 16:47:55 +0000

    Use requst package not urllib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-from urllib.request import urlopen, Request
+from requests import get
 """Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-    req = Request(url)
-    with urlopen(url) as response:
-        read = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(read.decode("UTF-8"))))
-        print('\t- content: {}'.format(read.decode("UTF-8")))
+    req = get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
