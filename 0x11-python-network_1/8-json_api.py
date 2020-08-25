cc14d1ede91e904c94c2af6cc103a9c9d9873e3c
Viet Tran <vietnamtran@gmail.com>
2020-08-25 01:40:59 +0000

    REfactor to make it more readable

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value
-    of the X-Request-Id variable found in the header of the response.
+"""Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 from requests import get, post
 from sys import argv
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    try:
-        arg = argv[1]
-    except IndexError:
-        print('No result')
-        quit()
+    data_dict = {}
+    if len(argv) < 2:
+        data_dict = {'q': ''}
+    else:
+        data_dict = {'q': argv[1]}
 
-    data_dict = {'q': arg}
     r = post(url, data=data_dict)
     try:
         if r.json() == {}:
             print('No result')
-        print('[{}] {}'.format(r.json()['id'], r.json()['name']))
+        else:
+            print('[{}] {}'.format(r.json()['id'], r.json()['name']))
     except:
         print('Not a valid JSON')
