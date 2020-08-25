b7e6d9d88de26333e6ff0b94cfbc8af6d7cc2d72
Viet Tran <vietnamtran@gmail.com>
2020-08-25 01:47:05 +0000

    Add proper Docstring

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value
-    of the X-Request-Id variable found in the header of the response.
+"""Takes in a URL, sends a request to the URL and an email address,
+   sends a POST request to the passed URL with the email as a parameter,
+   and finally displays the body of the response
 """
 from requests import post
 from sys import argv
