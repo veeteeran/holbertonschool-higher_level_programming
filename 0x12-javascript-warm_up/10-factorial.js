b9092d7e7445453252a2eef4e3caa5127e1898bb
Viet Tran <vietnamtran@gmail.com>
2020-09-15 18:57:39 +0000

    Tested code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function factorial (n) {
+  if (n === 1 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+const input = Number(process.argv[2]);
+console.log(factorial(input));
