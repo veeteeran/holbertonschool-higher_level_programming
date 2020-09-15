985f2c4515520115a4aa9f2ef63adca0b0056a49
Viet Tran <vietnamtran@gmail.com>
2020-09-15 17:44:48 +0000

    Tested code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+if (process.argv.length < 3 || !Number(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: %s', Math.floor(Number(process.argv[2])));
+}
