b3f91b6dd05a9a0d034289e6583aec7f90339dcd
Viet Tran <vietnamtran@gmail.com>
2020-09-15 19:48:11 +0000

    A little refactor, shrug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const array = [];
 const length = process.argv.length;
+process.argv.splice(0, 2);
 if (length > 3) {
-  for (let i = 2; i < length; i++) {
-    array.push(Number(process.argv[i]));
-  }
-  array.sort();
-  array.pop();
-  console.log(array.pop());
+  process.argv.sort(function (a, b) { return b - a; });
+  console.log(process.argv[1]);
 } else {
   console.log(0);
 }
