3f124793c5a5eb0a6d80a2d2df1a1741640bf58d
Viet Tran <vietnamtran@gmail.com>
2020-09-15 19:28:39 +0000

    Tested code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const array = [];
+const length = process.argv.length;
+if (length > 3) {
+  for (let i = 2; i < length; i++) {
+    array.push(Number(process.argv[i]));
+  }
+  array.sort();
+  array.pop();
+  console.log(array.pop());
+} else {
+  console.log(0);
+}
