6fed5b4c04d5ef5673189c16436fb5d8540fd21c
Viet Tran <vietnamtran@gmail.com>
2020-09-15 17:54:49 +0000

    Tested code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const index = process.argv[2];
+
+if (!Number(index)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < index; i++) {
+    console.log('C is fun');
+  }
+}
