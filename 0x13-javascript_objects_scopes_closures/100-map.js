0bbed7d53f181085b733619ebbf25c4bff483fd0
Viet Tran <vietnamtran@gmail.com>
2020-09-21 19:26:33 +0000

    Tested code

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+// imports an array and computes a new array
+const list = require('./100-data').list;
+console.log(list);
+const newList = list.map((x, index) => x * index);
+console.log(newList);
