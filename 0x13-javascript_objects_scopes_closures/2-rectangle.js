fe132d30f7cd523a82b131e5e16501629501e18d
Viet Tran <vietnamtran@gmail.com>
2020-09-21 17:32:04 +0000

    Tested code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 0 || w === undefined || h < 0 || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
