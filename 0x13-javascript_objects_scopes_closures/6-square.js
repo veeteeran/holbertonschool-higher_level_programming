0b27de76dea86b39d1fa432eb33230d4cb9604e2
Viet Tran <vietnamtran@gmail.com>
2020-09-21 18:13:42 +0000

    Tested code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // prints the rectangle using the character c
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
+      }
+    }
+  }
+};
