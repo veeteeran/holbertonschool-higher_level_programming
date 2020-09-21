8b68972221dc55f91ba8754686b25a09e1fab899
Viet Tran <vietnamtran@gmail.com>
2020-09-21 21:06:13 +0000

    Fix semistandard issue

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,13 +4,13 @@ imports a dictionary of occurrences by user id and computes
 a dictionary of user ids by occurrence
 */
 const dict = require('./101-data').dict;
-let newDict = {};
-for (let key in dict) {
-	let newKey = dict[key];
-	if (newKey in newDict) {
-		newDict[newKey].push(key)
-	} else {
-		newDict[newKey] = [key];
-	}
+const newDict = {};
+for (const key in dict) {
+  const newKey = dict[key];
+  if (newKey in newDict) {
+    newDict[newKey].push(key);
+  } else {
+    newDict[newKey] = [key];
+  }
 }
 console.log(newDict);
