adc7ec3aea133a218f2e1f9770c8d9b740a406ec
Viet Tran <vietnamtran@gmail.com>
2020-09-21 20:58:28 +0000

    Refactor with Jacob's help

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,23 +5,12 @@ a dictionary of user ids by occurrence
 */
 const dict = require('./101-data').dict;
 let newDict = {};
-const list1 = [];
-const list2 = [];
-const list3 = [];
-for (const key in dict) {
-  const value = dict[key];
-  if (value === 1) {
-    list1.push(key);
-  } else if (value === 2) {
-    list2.push(key);
-  } else {
-    list3.push(key);
-  }
+for (let key in dict) {
+	let newKey = dict[key];
+	if (newKey in newDict) {
+		newDict[newKey].push(key)
+	} else {
+		newDict[newKey] = [key];
+	}
 }
-
-newDict = {
-  1: list1,
-  2: list2,
-  3: list3
-};
 console.log(newDict);
