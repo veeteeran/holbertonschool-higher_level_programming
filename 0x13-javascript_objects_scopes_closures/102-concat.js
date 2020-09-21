46bb78965ccd83ca4a25bd59a55f504ed600f437
Viet Tran <vietnamtran@gmail.com>
2020-09-21 21:44:52 +0000

    Not passing semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.splice(2);
+fs.readFile(args[0], 'utf8', (err, data) => fs.writeFileSync(args[2], data));
+fs.readFile(args[1], 'utf8', (err, data) => fs.writeFileSync(args[2], data, { flag: 'a+' }));
