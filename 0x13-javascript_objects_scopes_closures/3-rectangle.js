ec9a447837b030277b261bc8ded7fa933351a1fe
Viet Tran <vietnamtran@gmail.com>
2020-09-21 17:46:24 +0000

    Tested code

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 0 || w === undefined || h < 0 || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+};
