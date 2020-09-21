96c6fe22bc8cec2a663b33e0bffb1031b54ce519
Viet Tran <vietnamtran@gmail.com>
2020-09-21 18:37:11 +0000

    Polluting the global namespace

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+// prints the number of arguments already printed and the new argument value
+var printed = 0;
+exports.logMe = function (item) {
+  console.log(printed + ': ' + item);
+  printed++;
+};
