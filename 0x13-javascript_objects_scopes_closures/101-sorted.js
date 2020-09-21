454636895bc2c14fcf0d901319ed9282c72bd30d
Viet Tran <vietnamtran@gmail.com>
2020-09-21 19:53:43 +0000

    Tested code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+/*
+imports a dictionary of occurrences by user id and computes
+a dictionary of user ids by occurrence
+*/
+const dict = require('./101-data').dict;
+let newDict = {};
+const list1 = [];
+const list2 = [];
+const list3 = [];
+for (const key in dict) {
+  const value = dict[key];
+  if (value === 1) {
+    list1.push(key);
+  } else if (value === 2) {
+    list2.push(key);
+  } else {
+    list3.push(key);
+  }
+}
+
+newDict = {
+  1: list1,
+  2: list2,
+  3: list3
+};
+console.log(newDict);
