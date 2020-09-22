d20e5267f5f6b947e8aa26b37f38cd1ba8ca81b8
Viet Tran <vietnamtran@gmail.com>
2020-09-22 15:58:19 +0000

    Tested code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.splice(2);
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    console.log('code: %d', response.statusCode);
+  }
+});
