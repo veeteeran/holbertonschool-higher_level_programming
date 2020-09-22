01f8a2e441fdced9e0dffaa7b300d9669d0bf0f5
Viet Tran <vietnamtran@gmail.com>
2020-09-22 17:43:43 +0000

    Tested code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const args = process.argv.splice(2);
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    fs.writeFileSync(args[1], body);
+  }
+});
