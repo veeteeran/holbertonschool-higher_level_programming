f07394bc4858db3a2494c88d226f8dafcb457144
Viet Tran <vietnamtran@gmail.com>
2020-09-22 17:06:17 +0000

    Tested code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.splice(2);
+let count = 0;
+const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    const swapi = JSON.parse(body);
+    for (let i = 0; i < swapi.results.length; i++) {
+      if (swapi.results[i].characters.includes(wedge)) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
