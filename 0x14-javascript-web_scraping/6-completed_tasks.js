fe922f4286e26cc2faf8f14a2f2259512fa701fd
Viet Tran <vietnamtran@gmail.com>
2020-09-22 22:30:12 +0000

    Add if task completed before creating key

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,7 +8,7 @@ request(args[0], function (error, response, body) {
   } else {
     const obj = JSON.parse(body);
     for (let i = 0; i < obj.length; i++) {
-      if (!returnDict[obj[i].userId]) {
+      if (!returnDict[obj[i].userId] && obj[i].completed) {
         returnDict[obj[i].userId] = 0;
       }
       if (obj[i].completed) {
