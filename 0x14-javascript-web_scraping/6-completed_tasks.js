a0997e9e196f3e21ae06f52531f76e513a358f63
Viet Tran <vietnamtran@gmail.com>
2020-09-22 18:14:42 +0000

    Tested code some ids are one off

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.splice(2);
+const returnDict = {};
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    const obj = JSON.parse(body);
+    let count = 0;
+    let userId = 1;
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i].completed) {
+        count++;
+        returnDict[obj[i].userId] = count;
+      }
+      if (obj[i].userId !== userId) {
+        count = 0;
+        userId++;
+      }
+    }
+  }
+  console.log(returnDict);
+});
