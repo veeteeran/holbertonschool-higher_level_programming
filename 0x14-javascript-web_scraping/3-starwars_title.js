b624c3a02dc7aaf0d45032503be58379c2400d27
Viet Tran <vietnamtran@gmail.com>
2020-09-22 16:19:36 +0000

    Tested code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.splice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[0];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    const obj = JSON.parse(body);
+    console.log(obj.title);
+  }
+});
