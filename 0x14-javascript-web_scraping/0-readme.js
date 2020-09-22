9399ec7323bf66acd9b537b67dde1947c6c025af
Viet Tran <vietnamtran@gmail.com>
2020-09-22 15:38:58 +0000

    Tested code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.splice(2);
+fs.readFile(args[0], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`${data}`);
+  }
+});
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.splice(2);
+fs.writeFileSync(args[0], args[1]);
