96b6d6f28e92fae8122ae709247716a3647aa8a9
Viet Tran <vietnamtran@gmail.com>
2020-09-22 21:14:05 +0000

    Tested code

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.splice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[0];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('code: %d', response.statusCode);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(ch => {
+      request(ch, function (error, response, body) {
+        if (error) {
+          console.log('code: %d', response.statusCode);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
