4c979f979cf0f4450f62a415b02ae9eb2dc9e19f
Viet Tran <vietnamtran@gmail.com>
2020-09-24 17:33:14 +0000

    Tested code

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/*
+adds a LI element to a list when the user clicks on the tag DIV#add_item
+*/
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    const item = '<li>Item</li>';
+    $('UL.my_list').append(item);
+  });
+});
