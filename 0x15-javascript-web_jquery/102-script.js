7ac9c75be8d5f48bde978f6d939d839365681136
Viet Tran <vietnamtran@gmail.com>
2020-09-24 21:43:39 +0000

    Tested code

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/*
+fetches and prints how to say “Hello” depending of the language
+*/
+$(document).ready(() => {
+  $('#btn_translate').click(() => {
+    const language = $('#language_code').val();
+    const url = `https://fourtonfish.com/hellosalut/?lang=${language}`;
+    $.get(url, data => $('#hello').text(data.hello));
+  });
+});
