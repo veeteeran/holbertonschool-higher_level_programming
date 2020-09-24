c32444ce95c5ffafbb14642e3361abb15966f351
Viet Tran <vietnamtran@gmail.com>
2020-09-24 21:02:32 +0000

    Tested code

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/*
+adds, removes and clears LI elements from a list when the user clicks
+*/
+$(document).ready(() => {
+  $('#add_item').click(() => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(() => {
+    $('UL.my_list li').eq(-1).remove();
+  });
+  $('#clear_list').click(() => {
+    $('UL.my_list').remove();
+  });
+});
