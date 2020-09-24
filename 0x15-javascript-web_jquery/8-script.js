e34f57f45d575ceb4c3c83d298e8eb7e284cf27b
Viet Tran <vietnamtran@gmail.com>
2020-09-24 19:42:54 +0000

    Tested code

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/*
+fetches and lists all movies title by using SWAPI
+*/
+$(document).ready(() => {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json',
+    data => data.results.forEach(result =>
+      $('UL#list_movies').append('<li>' + result.title + '</li>')));
+});
