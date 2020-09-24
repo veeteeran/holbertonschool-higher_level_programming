773b4cb54806ca094711156efabd64fdedb47554
Viet Tran <vietnamtran@gmail.com>
2020-09-24 20:29:32 +0000

    Tested code

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+/*
+updates the text color of the HTML tag HEADER to red (#FF0000)
+script must be imported from the HEAD tag, not at the end of the HTML
+*/
+document.addEventListener('DOMContentLoaded', () =>
+  document.querySelector('header').style.color = '#FF0000'
+);
