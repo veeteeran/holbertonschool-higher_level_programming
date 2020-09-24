e876989d14b974eb9992674ae1e52fe19307cf95
Viet Tran <vietnamtran@gmail.com>
2020-09-24 17:23:38 +0000

    Tested code

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* toggles the class of the HTML tag HEADER to red (#FF0000)
+when the user clicks on the tag DIV#toggle_header
+*/
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    } else {
+      $('header').removeClass('red');
+      $('header').addClass('green');
+    }
+  });
+});
