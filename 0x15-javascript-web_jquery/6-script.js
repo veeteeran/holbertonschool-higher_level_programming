77638820e993586064ba5cb058cd7aa5554578c9
Viet Tran <vietnamtran@gmail.com>
2020-09-24 17:45:31 +0000

    Tested code

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/*
+updates the text of the HTML tag HEADER to “New Header!!!”
+when the user clicks on DIV#update_header
+*/
+$(document).ready(() => {
+  $('#update_header').click(() =>
+    $('header').text('New Header!!!'));
+});
