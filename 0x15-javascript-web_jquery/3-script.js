e5e0732f54f48a6636b3bea1b70e4369e87de837
Viet Tran <vietnamtran@gmail.com>
2020-09-24 17:43:13 +0000

    Tested code

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+/*
+adds the class red to the HTML tag HEADER to red (#FF0000)
+when the user clicks on the tag DIV#red_header
+*/
 $(document).ready(function () {
   $('#red_header').click(function () {
     $('header').addClass('red');
