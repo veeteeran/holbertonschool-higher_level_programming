081705945b8a8850bd3b1c416c382637fae79646
Viet Tran <vietnamtran@gmail.com>
2020-09-25 01:09:05 +0000

    Just making sure

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-//updates the text color of the HTML tag HEADER to red using JQuery API
+// updates the text color of the HTML tag HEADER to red using JQuery API
 $(document).ready(() => $('header').css('color', '#FF0000'));
