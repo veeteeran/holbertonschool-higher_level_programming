2c3b311c431a4909ddb6375f17f721729221508c
Viet Tran <vietnamtran@gmail.com>
2020-09-25 00:52:10 +0000

    Refactor using toggleClass

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -4,12 +4,6 @@ when the user clicks on the tag DIV#toggle_header
 */
 $(document).ready(function () {
   $('#toggle_header').click(function () {
-    if ($('header').hasClass('green')) {
-      $('header').removeClass('green');
-      $('header').addClass('red');
-    } else {
-      $('header').removeClass('red');
-      $('header').addClass('green');
-    }
+    $('header').toggleClass('red green');
   });
 });
