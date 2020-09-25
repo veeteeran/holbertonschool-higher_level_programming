b425ba5ead334ef9bfea143adb0b121416b5d26b
Viet Tran <vietnamtran@gmail.com>
2020-09-25 01:04:53 +0000

    Tested code

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/*
+fetches from https://fourtonfish.com/hellosalut/?lang=fr
+and displays the value of hello from that fetch in the
+HTML’s tag DIV#hello
+*/
+$(document).ready(() => {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr',
+    data => $('#hello').text(data.hello));
+});
